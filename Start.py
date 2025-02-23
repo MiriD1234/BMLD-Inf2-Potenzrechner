bd366eb34047f3d4918aa67a9ab5a074c2f163f2
Miranda <downimir@students.zhaw.ch>
2025-02-23 13:56:32 +0100

    Namen und Emailadressen angepasst

--- Start.py
+++ Start.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 st.title("Meine erste Streamlit App")
 
-st.write("Diese App wurde von Anne Müller und Hans Meier entwickelt.")
+st.write("Diese App wurde von Elena Buchli (buchlele@students.zhaw.ch) und Miranda Downing (downimir@students.zhaw.ch) entwickelt.")
