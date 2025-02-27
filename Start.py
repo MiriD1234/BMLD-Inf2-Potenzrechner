8684750d0637cbe239af0ada7e1aef7300a8fd53
Miranda <downimir@students.zhaw.ch>
2025-02-27 11:23:44 +0100

    Text geändert

--- Start.py
+++ Start.py
@@ -4,3 +4,11 @@ import pandas as pd
 st.title("Meine erste Streamlit App")
 
 st.write("Diese App wurde von Elena Buchli (buchlele@students.zhaw.ch) und Miranda Downing (downimir@students.zhaw.ch) entwickelt.")
+
+"""Diese App wurde von folgenden Personen entwickelt:
+- Elena Buchli (buchlele@students.zhaw.ch)
+- Miranda Downing (downimir@students.zhaw.ch)
+
+Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
+
+Autor: Samuel Wehrli (wehs@zhaw.ch)"""
